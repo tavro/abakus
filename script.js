c332753748f29183d5d3eee9ac82c9ac05936881
Isak Horvath <48546805+tavro@users.noreply.github.com>
2023-10-10 18:36:50 +0200

    abakus-001: init

---
+++ script.js
@@ -0,0 +1,700 @@
+var words = {
+    abakus: {
+        ordklass: "substantiv",
+        betyder: "ett enklare räknehjälpmedel med en ram och ett antal ribbor med flyttbara kulor",
+        belagt: 1903,
+        från: [
+            {
+                ord: "abacus",
+                språk: "latin"
+            },
+            {
+                ord: "abax",
+                språk: "grekiska"
+            }
+        ]
+    },
+    abalienation: {
+        ordklass: "substantiv",
+        betyder: "avyttring av (fast) egendom",
+        belagt: 1650,
+        från: [
+            {
+                ord: "abalienatio",
+                språk: "latin"
+            }
+        ]
+    },
+    abasi: {
+        ordklass: "substantiv",
+        betyder: "oförmåga att gå",
+        belagt: 1989,
+        från: [
+            {
+                ord: "basis",
+                språk: "grekiska"
+            }
+        ]
+    },
+    abbé: {
+        ordklass: "substantiv",
+        betyder: "katolsk andlig person",
+        belagt: 1791,
+        från: [
+            {
+                ord: "abbé",
+                språk: "franska"
+            }
+        ]
+    },
+    abbedissa: {
+        ordklass: "substantiv",
+        betyder: "kvinnlig föreståndare för nunnekloster",
+        belagt: 1400,
+        från: [
+            {
+                ord: "abbatissa",
+                språk: "medeltidslatin"
+            }
+        ]
+    },
+    abborre: {
+        ordklass: "substantiv",
+        betyder: "en mindre söt- och brackvattensfisk med taggig främre ryggfena",
+        belagt: 1270,
+        från: [
+            {
+                ord: "aghborre",
+                språk: "fornsvenska"
+            }
+        ]
+    },
+    abbot: {
+        ordklass: "substantiv",
+        betyder: "manlig föreståndare för munkkloster",
+        belagt: 1355,
+        från: [
+            {
+                ord: "abba",
+                språk: "medeltidslatin"
+            }
+        ]
+    },
+    abbreviera: {
+        ordklass: "verb",
+        betyder: "skriva i förkortad form",
+        belagt: 1642,
+        från: [
+            {
+                ord: "abbreviare",
+                språk: "senlatin"
+            }
+        ]
+    },
+    abderitisk: {
+        ordklass: "adjektiv",
+        betyder: "enfaldig",
+        belagt: 1822,
+        från: [
+            {
+                ord: "abdera",
+                språk: "forngrekiska"
+            }
+        ]
+    },
+    abdikera: {
+        ordklass: "verb",
+        betyder: "avsäga sig kunglig eller kejserlig värdighet",
+        belagt: 1808,
+        från: [
+            {
+                ord: "abdiquer",
+                språk: "franska"
+            },
+            {
+                ord: "abdicare",
+                språk: "latin"
+            }
+        ]
+    },
+    abdomen: {
+        ordklass: "substantiv",
+        betyder: "bakkropp på leddjur",
+        belagt: 1835,
+        från: [
+            {
+                ord: "abdomen",
+                språk: "latin"
+            }
+        ]
+    },
+    abducera: {
+        ordklass: "verb",
+        betyder: "röra (en kroppsdel) från kroppens eller en kroppsdels mittlinje",
+        belagt: 1904,
+        från: [
+            {
+                ord: "abducere",
+                språk: "latin"
+            }
+        ]
+    },
+    abduktion: {
+        ordklass: "substantiv",
+        betyder: "rörelse av en kroppsdel åt sidan",
+        belagt: 1903,
+        från: [
+            {
+                ord: "abducere",
+                språk: "latin"
+            }
+        ]
+    },
+    aber: {
+        ordklass: "substantiv",
+        betyder: "(oväntat) hinder",
+        belagt: 1891,
+        från: [
+            {
+                ord: "aber",
+                språk: "tyska"
+            }
+        ]
+    },
+    aberration: {
+        ordklass: "substantiv",
+        betyder: "avbildningsfel",
+        belagt: 1875,
+        från: [
+            {
+                ord: "aberratio",
+                språk: "latin"
+            }
+        ]
+    },
+    abilitet: {
+        ordklass: "substantiv",
+        betyder: "grundläggande själslig förmåga",
+        belagt: 1968,
+        från: [
+            {
+                ord: "ability",
+                språk: "engelska"
+            },
+            {
+                ord: "habilitas",
+                språk: "latin"
+            }
+        ]
+    },
+    abiturient: {
+        ordklass: "substantiv",
+        betyder: "elev i gymnasiets avgångsklass som står i begrepp att avlägga studentexamen",
+        belagt: 1801,
+        från: [
+            {
+                ord: "abiturient",
+                språk: "tyska"
+            },
+            {
+                ord: "abiturire",
+                språk: "latin"
+            }
+        ]
+    },
+    ablation: {
+        ordklass: "substantiv",
+        betyder: "smältning och bortdunstning från glaciäryta",
+        belagt: 1903,
+        från: [
+            {
+                ord: "ablatio",
+                språk: "latin"
+            }
+        ]
+    },
+    ablativ: {
+        ordklass: "substantiv",
+        betyder: "ett kasus som främst anger riktning från något",
+        belagt: 1801,
+        från: [
+            {
+                ord: "ablativus",
+                språk: "latin"
+            }
+        ]
+    },
+    abnorm: {
+        ordklass: "adjektiv",
+        betyder: "som kraftigt avviker från det normala",
+        belagt: 1839,
+        från: [
+            {
+                ord: "abnormis",
+                språk: "latin"
+            }
+        ]
+    },
+    abolition: {
+        ordklass: "substantiv",
+        betyder: "efterskänkande av eventuellt straff",
+        belagt: 1844,
+        från: [
+            {
+                ord: "abolitio",
+                språk: "latin"
+            }
+        ]
+    },
+    abolitionism: {
+        ordklass: "substantiv",
+        betyder: "(rörelse som omfattade) ståndpunkten att slaveriet borde avskaffas",
+        belagt: 1856,
+        från: [
+            {
+                ord: "abolere",
+                språk: "latin"
+            }
+        ]
+    },
+    abonnemang: {
+        ordklass: "substantiv",
+        betyder: "på förhand gjord beställning för viss tid",
+        belagt: 1850,
+        från: [
+            {
+                ord: "abonnement",
+                språk: "franska"
+            }
+        ]
+    },
+    abonnera: {
+        ordklass: "verb",
+        betyder: "ha mer eller mindre säkrad tillgång",
+        belagt: 1779,
+        från: [
+            {
+                ord: "abonner",
+                språk: "franska"
+            }
+        ]
+    },
+    aborigin: {
+        ordklass: "substantiv",
+        betyder: "urinvånare i Australien",
+        belagt: "1960-talet",
+        från: [
+            {
+                ord: "aborigine",
+                språk: "engelska"
+            },
+            {
+                ord: "aborigines",
+                språk: "latin"
+            }
+        ]
+    },
+    abort: {
+        ordklass: "substantiv",
+        betyder: "avbrytande av havandeskap",
+        belagt: 1838,
+        från: [
+            {
+                ord: "abortus",
+                språk: "latin"
+            }
+        ]
+    },
+    abortera: {
+        ordklass: "verb",
+        betyder: "(låta) framkalla missfall",
+        belagt: 1838,
+        från: [
+            {
+                ord: "abortieren",
+                språk: "tyska"
+            },
+            {
+                ord: "avorter",
+                språk: "franska"
+            }
+        ]
+    },
+    abortiv: {
+        ordklass: "adjektiv",
+        betyder: "som har att göra med abort",
+        belagt: 1837,
+        från: [
+            {
+                ord: "abortivus",
+                språk: "latin"
+            }
+        ]
+    },
+    abradera: {
+        ordklass: "verb",
+        betyder: "undergå abrasion",
+        belagt: 1946,
+        från: [
+            {
+                ord: "abradere",
+                språk: "latin"
+            }
+        ]
+    },
+    abrakadabra: {
+        ordklass: "substantiv",
+        betyder: "(skenbart) meningslös ljudföljd med (förment) magisk verkan",
+        belagt: 1798,
+        från: [
+            {
+                ord: "abracadabra",
+                språk: "latin"
+            }
+        ]
+    },
+    abrupt: {
+        ordklass: "adjektiv",
+        betyder: "plötsligt inledd eller avslutad",
+        belagt: 1843,
+        från: [
+            {
+                ord: "abruptus",
+                språk: "latin"
+            }
+        ]
+    },
+    abscess: {
+        ordklass: "substantiv",
+        betyder: "varig, väl avgränsad, ofta utbuktande, bildning i huden",
+        belagt: 1837,
+        från: [
+            {
+                ord: "abscessus",
+                språk: "latin"
+            }
+        ]
+    },
+    absens: {
+        ordklass: "substantiv",
+        betyder: "kortvarig medvetslöshet",
+        belagt: 1629,
+        från: [
+            {
+                ord: "absentia",
+                språk: "latin"
+            }
+        ]
+    },
+    absid: {
+        ordklass: "substantiv",
+        betyder: "rumsavslutande utbyggnad på kyrka",
+        belagt: 1854,
+        från: [
+            {
+                ord: "abside",
+                språk: "franska"
+            },
+            {
+                ord: "absis",
+                språk: "latin"
+            },
+            {
+                ord: "hapsis",
+                språk: "grekiska"
+            }
+        ]
+    },
+    absint: {
+        ordklass: "substantiv",
+        betyder: "en grönaktig, starkt sprithaltig likör som bereds på malört m.m.",
+        belagt: 1871,
+        från: [
+            {
+                ord: "absinthe",
+                språk: "franska"
+            },
+            {
+                ord: "absinthium",
+                språk: "latin"
+            }
+        ]
+    },
+    abskissa: {
+        ordklass: "substantiv",
+        betyder: "koordinat som anger en punkts avstånd till lodräta axeln i koordinatsystem",
+        belagt: 1860,
+        från: [
+            {
+                ord: "abscissa",
+                språk: "latin"
+            }
+        ]
+    },
+    absolut: {
+        ordklass: "adjektiv",
+        betyder: "oberoende av all relation till något annat",
+        belagt: 1723,
+        från: [
+            {
+                ord: "absolutus",
+                språk: "latin"
+            }
+        ]
+    },
+    absolution: {
+        ordklass: "substantiv",
+        betyder: "syndaförlåtelse",
+        belagt: 1617,
+        från: [
+            {
+                ord: "absolutio",
+                språk: "latin"
+            }
+        ]
+    },
+    absolvera: {
+        ordklass: "verb",
+        betyder: "fullborda",
+        belagt: 1693,
+        från: [
+            {
+                ord: "absolvere",
+                språk: "latin"
+            }
+        ]
+    },
+    absorbera: {
+        ordklass: "verb",
+        betyder: "ta upp",
+        belagt: 1768,
+        från: [
+            {
+                ord: "absorbere",
+                språk: "latin"
+            }
+        ]
+    },
+    abstinens: {
+        ordklass: "substantiv",
+        betyder: "avhållsamhet",
+        belagt: 1876,
+        från: [
+            {
+                ord: "abstinentia",
+                språk: "latin"
+            }
+        ]
+    },
+    abstract: {
+        ordklass: "substantiv",
+        betyder: "kort sammanfattning",
+        belagt: 1960,
+        från: [
+            {
+                ord: "abstract",
+                språk: "engelska"
+            }
+        ]
+    },
+    abstrahera: {
+        ordklass: "verb",
+        betyder: "skapa en allmän och övergripande föreställning (om något)",
+        belagt: 1737,
+        från: [
+            {
+                ord: "abstrahere",
+                språk: "latin"
+            }
+        ]
+    },
+    abstrakt: {
+        ordklass: "adjektiv",
+        betyder: "som inte kan uppfattas med sinnena",
+        belagt: 1779,
+        från: [
+            {
+                ord: "abstractus",
+                språk: "latin"
+            }
+        ]
+    },
+    abstrus: {
+        ordklass: "adjektiv",
+        betyder: "dunkelt uttryckt",
+        belagt: 1716,
+        från: [
+            {
+                ord: "abstrusus",
+                språk: "latin"
+            }
+        ]
+    },
+    absurd: {
+        ordklass: "adjektiv",
+        betyder: "som strider mot lagarna för förnuftigt tänkande",
+        belagt: 1634,
+        från: [
+            {
+                ord: "absurdus",
+                språk: "latin"
+            }
+        ]
+    },
+    absurdism: {
+        ordklass: "substantiv",
+        betyder: "en (litterär) riktning som söker konstnärliga uttryck för tillvarons meningslöshet",
+        belagt: 1959,
+        från: [
+            {
+                ord: "absurdisme",
+                språk: "franska"
+            }
+        ]
+    },
+    absurditet: {
+        ordklass: "substantiv",
+        betyder: "det att något strider mot lagarna för förnuftigt tänkande",
+        belagt: 1650,
+        från: [
+            {
+                ord: "absurdität",
+                språk: "tyska"
+            },
+            {
+                ord: "absurditas",
+                språk: "latin"
+            }
+        ]
+    },
+    abundans: {
+        ordklass: "substantiv",
+        betyder: "mycket riklig tillgång",
+        belagt: 1886,
+        från: [
+            {
+                ord: "abundantia",
+                språk: "latin"
+            }
+        ]
+    },
+    abyssal: {
+        ordklass: "adjektiv",
+        betyder: "som tillhör de största djupen i hav",
+        belagt: 1896,
+        från: [
+            {
+                ord: "abyssal",
+                språk: "franska"
+            },
+            {
+                ord: "abyssus",
+                språk: "latin"
+            }
+        ]
+    },
+    MALL: {
+        ordklass: "",
+        betyder: "",
+        belagt: 0,
+        från: [
+            {
+                ord: "",
+                språk: ""
+            }
+        ]
+    }
+};
+
+function displayWord(w) {
+    var etymologiDiv = document.getElementById("etymologi");
+
+    var etym = "<h2 class=" + "lgray" + ">";
+    words[w].från.forEach(function (item) {
+        etym += "<span>" + item.ord + " <small><i><b>(" + item.språk + ")</b></i></small> </span>";
+    });
+    etym += " -></h2>";
+
+    var html = etym + "<h1 class=" + "word " + ">" + w + " <small class=" + "gray" + "><i>(" + words[w].belagt + ")</i></small></h1>" +
+        "<p class=" + "dgray" + ">" + words[w].betyder + " <small><i><b>(" + words[w].ordklass + ")</b></i></small></p><hr>";
+
+    for (let key in words) {
+        if (words.hasOwnProperty(key)) {
+            var id = "link-" + key;
+            html += "<a href=" + "#" + " id=" + id + ">" + key + "</a> "
+        }
+    }
+
+    etymologiDiv.innerHTML = html;
+
+    for (let key in words) {
+        if (words.hasOwnProperty(key)) {
+            var id = "link-" + key;
+            const link = document.getElementById(id);
+        
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+                displayWord(key);
+            });
+        }
+    }
+}
+
+function getWordCountByYear() {
+    const wordCountByYear = {};
+
+    Object.keys(words).forEach(word => {
+        const entry = words[word];
+        const { belagt } = entry;
+
+        if (belagt > 0 && word.length > 0) {
+            if (!wordCountByYear[belagt]) {
+                wordCountByYear[belagt] = 1;
+            } else {
+                wordCountByYear[belagt]++;
+            }
+        }
+    });
+
+    return wordCountByYear;
+}
+
+function getWordsByLanguage() {
+    const wordsByLanguage = new Map();
+
+    Object.keys(words).forEach(word => {
+        const entry = words[word];
+        const languages = entry.från;
+
+        languages.forEach(languageObj => {
+            const { språk, ord } = languageObj;
+
+            if (språk && språk.length > 0 && ord && ord.length > 0) {
+                if (!wordsByLanguage.has(språk)) {
+                    wordsByLanguage.set(språk, [ord]);
+                } else {
+                    wordsByLanguage.get(språk).push(ord);
+                }
+            }
+        });
+    });
+
+    return wordsByLanguage;
+}
+
+function getWordCountByLanguage() {
+    const wordsByLanguage = getWordsByLanguage(words);
+    const wordCountByLanguage = {};
+
+    wordsByLanguage.forEach((wordsArray, language) => {
+        const wordCount = wordsArray.length;
+        wordCountByLanguage[language] = wordCount;
+    });
+
+    return wordCountByLanguage;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    displayWord("abakus");
+    console.log(getWordCountByLanguage());
+});
